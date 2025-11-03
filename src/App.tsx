b5b4d93d5179e049b5abc68ba262158e6026cfb6
Yana Ovcharyk <yanaovcharik@gmail.com>
2025-11-03 15:53:22 +0200

    initial commit: base project structure with React, Vite, TypeScript

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { SearchForm } from "./components/SearchForm/SearchForm";
+
+function App() {
+  return (
+    <div style={{ display: "flex", justifyContent: "center" }}>
+      <SearchForm />
+    </div>
+  );
+}
+
+export default App;
